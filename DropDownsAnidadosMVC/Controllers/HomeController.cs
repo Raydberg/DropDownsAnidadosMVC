ebf7dc31aa6765d4449d6e223f25b2ba2fca4aeb
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-06-01 20:44:26 -0500

    Final Project

--- DropDownsAnidadosMVC/Controllers/HomeController.cs
+++ DropDownsAnidadosMVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using DropDownsAnidadosMVC.Models;
+﻿using DropDownsAnidadosMVC.Datos;
+using DropDownsAnidadosMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +8,20 @@ namespace DropDownsAnidadosMVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private ApplicationDbContext _contexto;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext contexto)
         {
             _logger = logger;
+            _contexto = contexto;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var sucursales = _contexto.Sucursal.ToList();
+            var vm = new DropDownsVM
+            { Sucursales = sucursales };
+            return View(vm);
         }
 
         public IActionResult Privacy()
@@ -28,5 +34,21 @@ namespace DropDownsAnidadosMVC.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+        [HttpGet]
+        public JsonResult ObtenerCategorias(int sucursalID)
+        {
+            var categorias = _contexto.Categoria.Where(c=>c.SucursalId == sucursalID).ToList();
+            return Json(categorias);
+        }
+
+        [HttpGet]
+        public JsonResult ObtenerProductos(int categoriaID)
+        {
+            var productos = _contexto.Producto.Where(p => p.CategoriaId == categoriaID).ToList();
+            return Json(productos);
+        }
+
     }
 }
---
+++ DropDownsAnidadosMVC/Models/DropDownsVM.cs
@@ -0,0 +1,7 @@
+﻿namespace DropDownsAnidadosMVC.Models
+{
+    public class DropDownsVM
+    {
+        public List<Sucursal> Sucursales { get; set; }
+    }
+}
